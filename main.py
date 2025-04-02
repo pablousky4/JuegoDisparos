fdf3804c20e52dc7ff810006fa63044221668def
Pxblo <69274746+pablousky4@users.noreply.github.com>
2025-04-02 07:58:36 +0000

    feat: agregar clases Boss y Opponent, implementar lógica de juego y actualizar recursos gráficos

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-from Game import Game
+import pygame
+from game import Game
+
+def main():
+    pygame.init()
+    game = Game()
+    game.run()
 
 if __name__ == "__main__":
-    game = Game()
-    game.start()
-    game.spawn_player("Player1")
-    game.spawn_opponent(is_star=True)
-    game.update_opponent()
-    game.update_player()
-    game.player.shoot()
-    game.opponent.shoot()
-    game.update()
-    game.end_game()
-    game.reset()
+    main()
